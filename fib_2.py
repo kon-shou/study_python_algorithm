ed5d671415cfe634a85ed0800af3fa15520c4730
Shohei Kondo <s.kondo1015@gmail.com>
2020-01-26 18:49:33 +0900

    フィボナッチ高速

--- fib_2.py
+++ fib_2.py
@@ -1,7 +1,24 @@
+memo = {1: 1, 2: 1}
+
+
 def fib(n):
-    if (n == 1) or (n == 2):
-        return 1
-    return fib(n - 2) + fib(n - 1)
+    if n in memo:
+        return memo[n]
+
+    memo[n] = fib(n - 2) + fib(n - 1)
+    return memo[n]
 
 
-print(fib(700))
+# print(fib(10))
+# print(memo.values())
+
+
+def fib_loop(n):
+    fib_arr = [1, 1]
+    for i in range(2, n):
+        fib_arr.append(fib_arr[i - 2] + fib_arr[i - 1])
+
+    return fib_arr[n - 1]
+
+
+print(fib_loop(100))
