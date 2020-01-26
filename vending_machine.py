4f8d787bf588140607bfa9b9be09a639cb50698e
Shohei Kondo <s.kondo1015@gmail.com>
2020-01-26 16:23:56 +0900

    forループに改良

--- vending_machine.py
+++ vending_machine.py
@@ -4,35 +4,9 @@ product_price = input('product: ')
 change = int(insert_price) - int(product_price)
 print(change)
 
-# 5000円
-r5000 = change // 5000
-q5000 = change % 5000
-print('5000: ' + str(r5000))
+coin = [5000, 1000, 500, 100, 50, 10, 5, 1]
 
-r1000 = q5000 // 1000
-q1000 = q5000 % 1000
-print('1000: ' + str(r1000))
-
-r500 = q1000 // 500
-q500 = q1000 % 500
-print('500: ' + str(r500))
-
-r100 = q500 // 100
-q100 = q500 % 100
-print('100: ' + str(r100))
-
-r50 = q100 // 50
-q50 = q100 % 50
-print('50: ' + str(r50))
-
-r10 = q50 // 10
-q10 = q50 % 10
-print('10: ' + str(r10))
-
-r5 = q10 // 5
-q5 = q10 % 5
-print('5: ' + str(r5))
-
-print('1: ' + str(q5))
-
-
+for i in coin:
+    r = change // i
+    change %= i
+    print(str(i) + ': ' + str(r))
