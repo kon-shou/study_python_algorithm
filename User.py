7c287147334fb8b4b9e54e35f1be8df1dd2e7d74
Shohei Kondo <s.kondo1015@gmail.com>
2020-01-26 15:56:00 +0900

    class

---
+++ User.py
@@ -0,0 +1,29 @@
+class User:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+    @staticmethod
+    def logout():
+        print('logout')
+
+    def login(self, password):
+        if self.password == password:
+            return True
+        else:
+            return False
+
+
+User.logout()
+
+a = User('admin', 'pass')
+
+if a.login('pass'):
+    print('成功')
+else:
+    print('失敗')
+
+if a.login('pass_no'):
+    print('成功')
+else:
+    print('失敗')
