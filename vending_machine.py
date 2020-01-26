1c80d7ace3507de29172d485947a600d30aab76f
Shohei Kondo <s.kondo1015@gmail.com>
2020-01-26 16:30:32 +0900

    エラーハンドリング

--- vending_machine.py
+++ vending_machine.py
@@ -1,7 +1,21 @@
+import sys
+
 # お釣り
 insert_price = input('insert: ')
+if not insert_price.isdecimal():
+    print('整数のみが許可されています')
+    sys.exit()
+
 product_price = input('product: ')
+if not product_price.isdecimal():
+    print('整数のみが許可されています')
+    sys.exit()
+
 change = int(insert_price) - int(product_price)
+if change < 0:
+    print('金額が不足しています')
+    sys.exit()
+
 print(change)
 
 coin = [5000, 1000, 500, 100, 50, 10, 5, 1]
