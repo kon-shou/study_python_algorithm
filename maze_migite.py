3c41b17cb36181f309191a5318b0deffd7a609ce
shohei kondo <kondo@s.kondo1015@gmail.com>
2020-03-17 20:45:49 +0900

    右手法の改良

--- maze_migite.py
+++ maze_migite.py
@@ -14,39 +14,47 @@ maze = [
 ]
 
 dir = [
-    [1, 0],   # 下
-    [0, 1],   # 右
-    [-1, 0],  # 上
-    [0, -1],  # 左
+    {'x': 0, 'y': 1},  # 下
+    {'x': 1, 'y': 0},  # 右
+    {'x': 0, 'y': -1},  # 上
+    {'x': -1, 'y': 0},  # 左
 ]
 
+x, y, depth = 1, 1, 0
 # 最初は下を向いている
-vertical_i, horizontal_i, depth, facing_direction = 1, 1, 0, 0
+facing_direction = 0
 
-while maze[vertical_i][horizontal_i] != 1:
-    maze[vertical_i][horizontal_i] = 2
+while maze[y][x] != 1:
+    maze[y][x] = 2
 
-    print('現在地: ', vertical_i, horizontal_i)
-    print('向いてる方向: ', facing_direction)
+    print('現在地 x:' + str(x) + ' y:' + str(y))
+    if facing_direction == 0:
+        print('向いてる方向: 下')
+    elif facing_direction == 1:
+        print('向いてる方向: 右')
+    elif facing_direction == 2:
+        print('向いてる方向: 上')
+    elif facing_direction == 3:
+        print('向いてる方向: 左')
 
     # 「現在向いている方向」を基準に、右手→正面→左手→背面の順に、進めるかを試行する
-    for next_direction, dir_value in enumerate([
+    for _, dir_value in enumerate([
         dir[(facing_direction - 1) % 4],  # 右手
         dir[facing_direction],            # 正面
         dir[(facing_direction + 1) % 4],  # 左手
         dir[(facing_direction + 2) % 4]   # 背面
     ]):
-        if maze[vertical_i + dir_value[0]][horizontal_i + dir_value[1]] < 2:
-            vertical_i += dir_value[0]
-            horizontal_i += dir_value[1]
+        if maze[y + dir_value['y']][x + dir_value['x']] < 2:
+            y += dir_value['y']
+            x += dir_value['x']
             # 進んだ歩数から、移動後に向いてる方向を求める
             facing_direction = dir.index(dir_value)
             depth += 1
             break
         # 移動できない and 既に移動した場所が存在する場合、移動した場所に戻る
-        elif maze[vertical_i + dir_value[0]][horizontal_i + dir_value[1]] == 2:
-            vertical_i += dir_value[0]
-            horizontal_i += dir_value[1]
+        elif maze[y + dir_value['y']][x + dir_value['x']] == 2:
+            y += dir_value['y']
+            x += dir_value['x']
             facing_direction = dir.index(dir_value)
             depth -= 1
             break
